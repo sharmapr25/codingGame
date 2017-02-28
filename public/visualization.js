0019b0a2dab0286c5b1e3c681603301576c9c3b8
piolAtif <spreeti@thoughtworks.com>
2017-03-01 01:05:51 +0530

    Refactored encodeMsg method

--- public/visualization.js
+++ public/visualization.js
@@ -1,5 +1,5 @@
 var showChuckNorris = function(){
 	var message = document.getElementsByName('string')[0].value;
-	var encoded = encodedMsg(message);
+	var encoded = encodeMsg(message);
 	document.getElementsByClassName("chuckNorrisMessage")[0].innerHTML = encoded;
 }
